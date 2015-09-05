acebbafa695de94527049f7d0f29dfce22497ced
Daniel Carter <danielchriscarter@gmail.com>
2015-09-05 18:30:01 +0100

    Added working display

--- js/mandelbrot.js
+++ js/mandelbrot.js
@@ -1,7 +1,20 @@
+function mandelbrotSet(c_real, c_img, iter)
+{
+	var x_real = c_real;
+	var x_img = c_img;
+	for(var i = 0; i<iter; i++)
+	{
+		var re = (x_real*x_real)-(x_img*x_img) + c_real;
+		var im = (2 * x_real * x_img) + c_img;
+		x_real = re;
+		x_img = im;
+	}
+	return ((x_real*x_real)+(x_img*x_img))<1;
+};
+
 window.onload = function() {
     var canvas = document.getElementById("mandelbrot");
     var context = canvas.getContext("2d");
-    context.fillRect(0,0,200,200);
 
     var x0 = -2.0;
     var y0 = -1.0;
@@ -11,6 +24,9 @@ window.onload = function() {
         for (var j = 0; j < canvas.height; j++) {
             var x = x0 + i * width / canvas.width;
             var y = y0 + j * height / canvas.height;
+			if(mandelbrotSet(x,y,100)) {
+				context.fillRect(i,j,1,1);
+			}
         }
     }
-}
+};
